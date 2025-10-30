e40474a315d0c45ad4379bc6ce14b26ffd335030
NhatTai <nhattai186@gmail.com>
2025-10-30 21:20:58 +0700

    sua cau 5 chuong 5

--- Chuong5_Cau5.py
+++ Chuong5_Cau5.py
@@ -9,22 +9,26 @@ nguyenam=0
 phuam=0
 NguyenAm="aăâeêioôơuưy"
 PhuAm="bcdđghklmnpqrstvx"
-
 for i in s :
     if i.isupper() :
         hoa+=1
+        if i.lower() in NguyenAm :
+            nguyenam+=1
+        elif i.lower() in PhuAm :
+            phuam+=1
     elif i.islower() :
         thuong+=1
+        if i in NguyenAm :
+            nguyenam+=1
+        elif i in PhuAm :
+            phuam+=1
     elif i.isdigit() :
         so+=1
     elif i in string.punctuation :
         kytudatbiet+=1
     elif i==' ':
         khoangtrang+=1
-    elif i.lower in NguyenAm :
-        nguyenam+=1
-    elif i.lower in PhuAm :
-        phuam+=1
+
 print("Số lương chữ hoa là :",hoa)
 print("Số lượng chữ thường là :",thuong)
 print("Số lượng số là :",so)
